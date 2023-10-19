402dad823d286cf5bb3de4e73720af734b8c1d2e
JollyFrankle <jollyfrankle3@gmail.com>
2023-10-19 22:17:18 +0700

    + home page: 10% finished

--- src/pages/public/_layout/LayoutHome.tsx
+++ src/pages/public/_layout/LayoutHome.tsx
@@ -42,67 +42,71 @@ const components: { title: string; href: string; description: string }[] = [
 
 export default function LayoutHome() {
     return <>
-        <NavigationMenu>
-            <NavigationMenuList>
-                <NavigationMenuItem>
-                    <NavigationMenuTrigger>Getting started</NavigationMenuTrigger>
-                    <NavigationMenuContent>
-                        <ul className="grid gap-3 p-6 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
-                            <li className="row-span-3">
-                                <NavigationMenuLink asChild>
-                                    <a
-                                        className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
-                                        href="/"
-                                    >
-                                        <FontAwesomeIcon icon={faReact} className="text-6xl" />
-                                        <div className="mb-2 mt-4 text-lg font-medium">
-                                            shadcn/ui
-                                        </div>
-                                        <p className="text-sm leading-tight text-muted-foreground">
-                                            Beautifully designed components built with Radix UI and
-                                            Tailwind CSS.
-                                        </p>
-                                    </a>
-                                </NavigationMenuLink>
-                            </li>
-                            <ListItem href="/docs" title="Introduction">
-                                Re-usable components built using Radix UI and Tailwind CSS.
-                            </ListItem>
-                            <ListItem href="/docs/installation" title="Installation">
-                                How to install dependencies and structure your app.
-                            </ListItem>
-                            <ListItem href="/docs/primitives/typography" title="Typography">
-                                Styles for headings, paragraphs, lists...etc
-                            </ListItem>
-                        </ul>
-                    </NavigationMenuContent>
-                </NavigationMenuItem>
-                <NavigationMenuItem>
-                    <NavigationMenuTrigger>Components</NavigationMenuTrigger>
-                    <NavigationMenuContent>
-                        <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
-                            {components.map((component) => (
-                                <ListItem
-                                    key={component.title}
-                                    title={component.title}
-                                    href={component.href}
-                                >
-                                    {component.description}
+        <section className='fixed top-0 w-full top-nav py-4 z-50'>
+            <div className="container">
+            <NavigationMenu>
+                <NavigationMenuList>
+                    <NavigationMenuItem>
+                        <NavigationMenuTrigger>Getting started</NavigationMenuTrigger>
+                        <NavigationMenuContent>
+                            <ul className="grid gap-3 p-6 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
+                                <li className="row-span-3">
+                                    <NavigationMenuLink asChild>
+                                        <a
+                                            className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
+                                            href="/"
+                                        >
+                                            <FontAwesomeIcon icon={faReact} className="text-6xl" />
+                                            <div className="mb-2 mt-4 text-lg font-medium">
+                                                shadcn/ui
+                                            </div>
+                                            <p className="text-sm leading-tight text-muted-foreground">
+                                                Beautifully designed components built with Radix UI and
+                                                Tailwind CSS.
+                                            </p>
+                                        </a>
+                                    </NavigationMenuLink>
+                                </li>
+                                <ListItem href="/docs" title="Introduction">
+                                    Re-usable components built using Radix UI and Tailwind CSS.
                                 </ListItem>
-                            ))}
-                        </ul>
-                    </NavigationMenuContent>
-                </NavigationMenuItem>
-                <NavigationMenuItem asChild>
-                    <NavigationMenuLink className={navigationMenuTriggerStyle()} asChild>
-                        {/* asChild: semua prop element ini akan di pass ke element child yg ada di dalamnya */}
-                        <Link to="/docs">
-                            Dokumentasi
-                        </Link>
-                    </NavigationMenuLink>
-                </NavigationMenuItem>
-            </NavigationMenuList>
-        </NavigationMenu>
+                                <ListItem href="/docs/installation" title="Installation">
+                                    How to install dependencies and structure your app.
+                                </ListItem>
+                                <ListItem href="/docs/primitives/typography" title="Typography">
+                                    Styles for headings, paragraphs, lists...etc
+                                </ListItem>
+                            </ul>
+                        </NavigationMenuContent>
+                    </NavigationMenuItem>
+                    <NavigationMenuItem>
+                        <NavigationMenuTrigger>Components</NavigationMenuTrigger>
+                        <NavigationMenuContent>
+                            <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
+                                {components.map((component) => (
+                                    <ListItem
+                                        key={component.title}
+                                        title={component.title}
+                                        href={component.href}
+                                    >
+                                        {component.description}
+                                    </ListItem>
+                                ))}
+                            </ul>
+                        </NavigationMenuContent>
+                    </NavigationMenuItem>
+                    <NavigationMenuItem asChild>
+                        <NavigationMenuLink className={navigationMenuTriggerStyle()} asChild>
+                            {/* asChild: semua prop element ini akan di pass ke element child yg ada di dalamnya */}
+                            <Link to="/docs">
+                                Dokumentasi
+                            </Link>
+                        </NavigationMenuLink>
+                    </NavigationMenuItem>
+                </NavigationMenuList>
+            </NavigationMenu>
+            </div>
+        </section>
 
         <Outlet />
 
--- src/pages/public/home/PageHome.tsx
+++ src/pages/public/home/PageHome.tsx
@@ -1,25 +1,71 @@
 import { Swiper, SwiperSlide } from "swiper/react"
 import "swiper/css"
+import 'swiper/css/effect-creative';
+
+import { EffectCreative, Navigation } from "swiper/modules"
+
+import CoverImage from "@/assets/images/cover-image.jpg"
+import { Card, CardContent } from "@/cn/components/ui/card";
 
 export default function PageHome() {
-    return <Swiper
-        style={{ height: '80vh'}}
-        loop={true}
-    >
-        <SwiperSlide>
-            <div className="container">
-                <div className="row">
-                    <div className="col-12 col-md-6">
-                        <h1 className="text-4xl font-bold">shadcn/ui</h1>
-                        <p className="text-xl">A collection of accessible, reusable, and composable React components for building interactive user interfaces.</p>
-                    </div>
-                    <div className="col-12 col-md-6">
-                        {/* <img src="https://raw.githubusercontent.com/shadcn/ui/main/docs/assets/hero.svg" alt="shadcn/ui" /> */}
+    return <>
+        <section className="h-[80vh] w-full relative">
+            <img src={CoverImage} className="absolute top-0 left-0 w-full h-full pointer-events-none rounded-br-3xl rounded-bl-3xl shadow-lg object-cover" />
+            <div className="absolute left-0 w-full z-10 bottom-28">
+                <div className="container">
+                    <h2 className="text-6xl mb-2 font-bold text-primary-foreground"><mark>Grand Atma</mark> Hotel</h2>
+                    <h3 className="text-4xl text-primary-foreground">tempat <mark>ternyaman</mark> Anda di Yogyakarta.</h3>
+                </div>
+            </div>
+            <div className="absolute left-0 w-full h-96 bottom-0 bg-gradient-to-t from-[#0000005d] to-transparent rounded-br-3xl rounded-bl-3xl"></div>
+            {/* a card 50% below this section to reserve room */}
+            <div className="absolute left-0 w-full md:h-32 md:-bottom-16">
+                <div className="container h-full">
+                    <Card className="w-full h-full rounded-3xl shadow-lg">
+                        <CardContent className="flex flex-row items-center justify-between py-4">
+                            123
+                        </CardContent>
+                    </Card>
+                </div>
+            </div>
+        </section>
+        <section className="container py-8 relative">
+            <div className="z-10 mx-auto max-w-screen-2xl my-4 md:my-8">
+                {/* <div className="pointer-events-none absolute left-0 top-0 -z-10 h-full w-full overflow-hidden bg-surface-1 shadow-2xl md:w-[calc(50%+32px)]"></div> */}
+                <div className="shadow-lg p-5 md:p-10 md:w-1/2">
+                    <div className="mt-8 text-5xl font-bold">Jogja...</div>
+                    <div className="my-2 mt-4 text-2xl">terbuat dari rindu, pulang, dan angkringan.</div>
+                    <div className="mt-6">
+                        <a className="inline-block rounded-lg bg-primary px-6 py-5 font-bold duration-200 hover:bg-primary-shade hover:no-underline active:rounded-xl text-primary-foreground" href="/get-started">Get Started</a>
                     </div>
                 </div>
             </div>
-        </SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-    </Swiper>
+            <Swiper
+                grabCursor={true}
+                effect={'creative'}
+                creativeEffect={{
+                    prev: {
+                        shadow: true,
+                        translate: [0, 0, -400],
+                      },
+                      next: {
+                        translate: ['100%', 0, 0],
+                      },
+                }}
+                navigation={true}
+                className="!z-20 mt-8 max-w-[740px] md:!absolute md:bottom-12 md:right-6 md:top-4 md:mt-0 md:w-1/2 lg:right-8 lg:top-12 xl:right-10"
+                modules={[EffectCreative, Navigation]}
+            >
+                <SwiperSlide className="bg-secondary">
+                    <img src="https://images.unsplash.com/photo-1566073771259-6a8506099945?auto=format&fit=crop&q=80&w=1000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fGhvdGVsfGVufDB8fDB8fHww" className="w-full h-full object-cover" />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src="https://images.unsplash.com/photo-1566073771259-6a8506099945?auto=format&fit=crop&q=80&w=1000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fGhvdGVsfGVufDB8fDB8fHww" className="w-full h-full object-cover" />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src="https://images.unsplash.com/photo-1566073771259-6a8506099945?auto=format&fit=crop&q=80&w=1000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fGhvdGVsfGVufDB8fDB8fHww" className="w-full h-full object-cover" />
+                </SwiperSlide>
+            </Swiper>
+        </section>
+    </>
 }
